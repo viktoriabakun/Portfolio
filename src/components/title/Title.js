740044654e6641b4d93374a8637539219d7038b4
viktoriabakun <viktoriabakun00@gmail.com>
2021-02-03 22:28:09 +0300

    add variables and mixins

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import s from './Title.module.css'
+import s from './Title.module.scss'
 
 function Title(props) {
     return (
-        <div>
+        <>
             <div className={s.title}>
                 <h2>{props.text}</h2>
             </div>
-        </div>
+        </>
 
     );
 }
