1f15f26ed4e66105fa8eb73da2c45525b60db9ae
viktoriabakun <viktoriabakun00@gmail.com>
2021-02-04 12:18:47 +0300

    install FontAwesome. Add icons for Skills

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -4,6 +4,14 @@ import sContainer from '../common/styles/Container.module.css'
 import Skill from './skill/Skill'
 import Title from "../components/title/Title";
 
+import {faReact} from "@fortawesome/free-brands-svg-icons";
+import {faHtml5} from "@fortawesome/free-brands-svg-icons";
+import {faJs} from "@fortawesome/free-brands-svg-icons";
+import {faCss3Alt} from "@fortawesome/free-brands-svg-icons";
+import {faServer, faSitemap} from "@fortawesome/free-solid-svg-icons";
+import typescript from '../assets/img/typescript.svg'
+
+
 
 function Skills() {
     return (
@@ -11,10 +19,28 @@ function Skills() {
             <div className={`${sContainer.container} ${s.skillsContainer}`}>
                 <Title text='Skills'/>
                 <div className={s.skills}>
-                    <Skill title={'JS'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. '}/>
-                    <Skill title={'React'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'}/>
-                    <Skill title={'HTML'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. '}/>
-                    <Skill title={'Css'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. '}/>
+
+                    <Skill title={'React'}
+                           icon={faReact}
+                           description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s...'}/>
+                    <Skill title={'Redux'}
+                           icon={faSitemap}
+                           description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s...'}/>
+                    <Skill title={'REST API'}
+                           icon={faServer}
+                           description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s...'}/>
+                    <Skill title={'TypeScript'}
+                           src={typescript}
+                           description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s...'}/>
+                    <Skill title={'JavaScript'}
+                           icon={faJs}
+                           description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s...'}/>
+                    <Skill title={'HTML'}
+                           icon={faHtml5}
+                           description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s...'}/>
+                    <Skill title={'Css'}
+                           icon={faCss3Alt}
+                           description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s...'}/>
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import s from './Skill.module.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
 
 function Skill(props) {
     return (
         <div className={s.skill}>
-            <div className={s.icon}></div>
+            <div className={s.icon}>
+                <FontAwesomeIcon icon={props.icon} size={'4x'} color={'#b796d9'}/>
+                <img src={props.src}/>
+            </div>
             <h3 className={s.skillTitle}>{props.title}</h3>
             <span className={s.skillDescription}>{props.description}</span>
         </div>
